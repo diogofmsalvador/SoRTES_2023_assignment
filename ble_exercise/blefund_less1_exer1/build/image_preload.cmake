7cc44cae209a9be297d380d2d6a746f70c9d7a15
diogofmsalvador <diogofmsalvador@gmail.com>
2023-11-28 15:10:19 +0100

    first commit

---
+++ ble_exercise/blefund_less1_exer1/build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD "adafruit_feather_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "/Users/dsalvador/Desktop/KU_LEUVEN/Software_for_Real_Time_and_Embedded_Systems/bt-fund/lesson1/blefund_less1_exer1;/opt/nordic/ncs/v2.5.0-rc2/nrf;/opt/nordic/ncs/v2.5.0-rc2/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/dsalvador/Desktop/KU_LEUVEN/Software_for_Real_Time_and_Embedded_Systems/bt-fund/lesson1/blefund_less1_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "boards/nrf52840dongle_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "boards/nrf52840dongle_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/4ef6631da0/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
