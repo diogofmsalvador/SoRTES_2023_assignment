7f41a9191b07d86ddfd813220694e536ec0335f6
diogofmsalvador <diogofmsalvador@gmail.com>
2023-11-28 15:59:22 +0100

    [DS] Added temperature reading - thread A

--- final_project/sender/src/main.c
+++ final_project/sender/src/main.c
@@ -4,47 +4,54 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/drivers/uart.h>
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp280;
 
 /* STEP 2 - Define stack size and scheduling priority used by each thread */
 #define STACKSIZE 1024 
-#define THREAD0_PRIORITY 7
-#define THREAD1_PRIORITY 7
-#define THREAD2_PRIORITY 7
+#define THREADA_PRIORITY 7
+#define THREADB_PRIORITY 7
+#define THREADC_PRIORITY 7
 
-void thread0(void)
+float temperature;
+
+void threadA(void)
 {
+	printk("Hello, I am thread0\n");
 	while (1) {
-		printk("Hello, I am thread0\n");
-		k_msleep(5);
+		temperature = bmp280.readTemperature();
+
+		printk("Temperature: ");
+  		printk(temperature);
 	}
 }
 
-void thread1(void)
+void threadB(void)
 {
+	printk("Hello, I am thread1\n");
 	while (1) {
-		  printk("Hello, I am thread1\n");
-		  k_msleep(5);
+
 	}
 }
 
-void thread2(void)
+void threadC(void)
 {
+	printk("Hello, I am thread2\n");
 	while (1) {
-		  printk("Hello, I am thread2\n");
-		  k_msleep(5);
+		
 	}
 }
 
-K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
-K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);
-K_THREAD_DEFINE(thread2_id, STACKSIZE, thread2, NULL, NULL, NULL, THREAD2_PRIORITY, 0, 0);
+K_THREAD_DEFINE(threadA_id, STACKSIZE, threadA, NULL, NULL, NULL, THREADA_PRIORITY, 0, 0);
+K_THREAD_DEFINE(threadB_id, STACKSIZE, threadB, NULL, NULL, NULL, THREADB_PRIORITY, 0, 0);
+K_THREAD_DEFINE(threadC_id, STACKSIZE, threadC, NULL, NULL, NULL, THREADC_PRIORITY, 0, 0);
 
-/* NOTE: main function required to enable USB communication */
+
 int main() {
 	if (usb_enable(NULL)) {
 		return 0;
 	}
-	/* Busy wait for 5 seconds so you don't miss the initial printk statements to the terminal while it is connecting */
-	/* Feel free to adjust longer or shorter */
+
 	k_busy_wait(5000000);
 }
