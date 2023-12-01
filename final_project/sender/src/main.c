6edc990503f0da5e550bd02262a5aba2fe99d7bc
diogofmsalvador <diogofmsalvador@gmail.com>
2023-12-01 23:25:05 +0100

    [DS] Added sender code for Bluetooth connection and final functionalities for threadA B and C

--- final_project/sender/src/main.c
+++ final_project/sender/src/main.c
@@ -4,153 +4,254 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/drivers/uart.h>
-
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
+
 #include <stdio.h>
 
-static double high_temp;
-static double low_temp;
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
 
-/* STEP 2 - Define stack size and scheduling priority used by each thread */
-#define STACKSIZE 1024 
-#define THREADA_PRIORITY 7
-#define THREADB_PRIORITY 7
-#define THREADC_PRIORITY 7
+// Bluetooth configuration variables
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
-int read_temperature(const struct device *dev, struct sensor_value *val)
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
 {
-	int ret;
+    if (err) {
+        printk("Connection failed (err %u)\n", err);
+        return;
+    }
 
-	ret = sensor_sample_fetch_chan(dev, SENSOR_CHAN_AMBIENT_TEMP);
-	if (ret < 0) {
-		printf("Could not fetch temperature: %d\n", ret);
-		return ret;
-	}
-
-	ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, val);
-	if (ret < 0) {
-		printf("Could not get temperature: %d\n", ret);
-	}
-	return ret;
+    printk("Connected\n");
 }
 
-void temp_alert_handler(const struct device *dev, const struct sensor_trigger *trig)
+static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	int ret;
-	struct sensor_value value;
-	double temp;
-
-	/* Read sensor value */
-	ret = read_temperature(dev, &value);
-	if (ret < 0) {
-		printf("Reading temperature failed: %d\n", ret);
-		return;
-	}
-	temp = sensor_value_to_double(&value);
-	if (temp <= low_temp) {
-		printf("Temperature below threshold: %0.1f°C\n", temp);
-	} else if (temp >= high_temp) {
-		printf("Temperature above threshold: %0.1f°C\n", temp);
-	} else {
-		printf("Error: temperature alert triggered without valid condition\n");
-	}
+    printk("Disconnected (reason %u)\n", reason);
 }
 
-void threadA(void)
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+static void security_changed(struct bt_conn *conn, bt_security_t level, enum bt_security_err err)
 {
-	printk("Hello, I am thread0\n");
-	
-	const struct device *const dev = DEVICE_DT_GET(DT_ALIAS(ambient_temp0));
-	struct sensor_value value;
-	double temp;
-	int ret;
-	const struct sensor_trigger trig = {
-		.chan = SENSOR_CHAN_AMBIENT_TEMP,
-		.type = SENSOR_TRIG_THRESHOLD,
-	};
+    char addr[BT_ADDR_LE_STR_LEN];
 
-	printf("Thermometer Example (%s)\n", CONFIG_ARCH);
+    bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	if (!device_is_ready(dev)) {
-		printf("Device %s is not ready\n", dev->name);
-	}
+    if (!err) {
+        printk("Security changed: %s level %u\n", addr, level);
+    } else {
+        printk("Security failed: %s level %u err %d\n", addr, level, err);
+    }
+}
+#endif
 
-	printf("Temperature device is %p, name is %s\n", dev, dev->name);
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+    .connected = connected,
+    .disconnected = disconnected,
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+    .security_changed = security_changed,
+#endif
+};
 
-	/* First, fetch a sensor sample to use for sensor thresholds */
-	ret = read_temperature(dev, &value);
-	if (ret != 0) {
-		printf("Failed to read temperature: %d\n", ret);
-	}
-	temp = sensor_value_to_double(&value);
+#if defined(CONFIG_BT_LBS_SECURITY_ENABLED)
+// Keep the authentication callbacks as they are relevant for Bluetooth security
+static struct bt_conn_auth_cb conn_auth_callbacks = {
+    // Callback functions here...
+};
 
-	/* Set thresholds to +0.5 and +1.5 °C from ambient */
-	low_temp = temp + 0.5;
-	ret = sensor_value_from_double(&value, low_temp);
-	if (ret != 0) {
-		printf("Failed to convert low threshold to sensor value: %d\n", ret);
-	}
-	ret = sensor_attr_set(dev, SENSOR_CHAN_AMBIENT_TEMP,
-			      SENSOR_ATTR_LOWER_THRESH, &value);
-	if (ret == 0) {
-		/* This sensor supports threshold triggers */
-		printf("Set temperature lower limit to %0.1f°C\n", low_temp);
-	}
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
+    // Callback functions here...
+};
+#else
+static struct bt_conn_auth_cb conn_auth_callbacks;
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+#endif
 
-	high_temp = temp + 1.5;
-	ret = sensor_value_from_double(&value, high_temp);
-	if (ret != 0) {
-		printf("Failed to convert low threshold to sensor value: %d\n", ret);
-	}
-	ret = sensor_attr_set(dev, SENSOR_CHAN_AMBIENT_TEMP,
-			      SENSOR_ATTR_UPPER_THRESH, &value);
-	if (ret == 0) {
-		/* This sensor supports threshold triggers */
-		printf("Set temperature upper limit to %0.1f°C\n", high_temp);
-	}
+#define STACKSIZE       1024
+#define THREADA_PRIORITY 4
+#define THREADB_PRIORITY 4
+#define THREADC_PRIORITY 4
 
-	ret = sensor_trigger_set(dev, &trig, temp_alert_handler);
-	if (ret == 0) {
-		printf("Enabled sensor threshold triggers\n");
-	}
+#define TEAM_NUMBER 01 // Replace with your team number
 
-	while (1) {
-		ret = read_temperature(dev, &value);
-		if (ret != 0) {
-			printf("Failed to read temperature: %d\n", ret);
-			break;
-		}
+static const struct gpio_dt_spec led_spec = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-		printf("Temperature is %0.1lf°C\n", sensor_value_to_double(&value));
+/* Define semaphore to monitor instances of available resource */
+K_SEM_DEFINE(instance_monitor_sem, 10, 10);
 
-		k_sleep(K_MSEC(1000));
-	}
+K_SEM_DEFINE(threadB_sem, 0, 1);
+
+volatile uint32_t available_instance_count = 10;
+
+/* Array to store last 10 temperature readings */
+struct sensor_value temp_readings[10];
+int reading_index = 0;
+
+/* Producer thread relinquishing access to instance */
+void threadA(void) {
+    printk("threadA started\n");
+    const struct device *dev;
+    int ret;
+    struct sensor_value temp_val;
+    int message_number = 1;
+
+    printf("Starting temperature sensor example\n");
+
+    dev = DEVICE_DT_GET_ANY(nordic_nrf_temp);
+    if (dev == NULL) {
+        printf("Could not get device for temperature sensor\n");
+        return;
+    }
+
+    if (!device_is_ready(dev)) {
+        printf("Sensor device is not ready\n");
+        return;
+    }
+
+    while (1) {
+        ret = sensor_sample_fetch(dev);
+        if (ret) {
+            printf("Failed to fetch sample from sensor: %d\n", ret);
+            k_sleep(K_SECONDS(1));
+            continue;
+        }
+
+        ret = sensor_channel_get(dev, SENSOR_CHAN_DIE_TEMP, &temp_val);
+        if (ret) {
+            printf("Failed to get sensor data: %d\n", ret);
+            k_sleep(K_SECONDS(1));
+            continue;
+        }
+
+        printf("Temperature: %d.%06dC\n", temp_val.val1, temp_val.val2);
+        printf("T%02d|%02d|%02d\n", TEAM_NUMBER, message_number, temp_val.val1);
+        message_number++;
+
+        // Store temperature reading
+        temp_readings[reading_index % 10] = temp_val;
+        reading_index++;
+
+        k_sleep(K_SECONDS(1));
+    }
 }
 
-void threadB(void)
-{
-	printk("Hello, I am thread1\n");
-	while (1) {
-	}
+/* Function to calculate average temperature */
+int calculate_average_temperature() {
+    long total = 0;
+    for (int i = 0; i < 10; i++) {
+        total += temp_readings[i].val1;
+    }
+    return total / 10;
 }
 
-void threadC(void)
-{
-	printk("Hello, I am thread2\n");
-	while (1) {
+void threadB(void) {
+    printk("threadB started\n");
+    int send_count = 0;
+    while (send_count < 20) {
+        if (reading_index >= 10) {
+            int average_temp = calculate_average_temperature();
+            printf("Average Temperature: %dC\n", average_temp);
+            printf("T%02d|%02d|%02d\n", TEAM_NUMBER, send_count + 1, average_temp);
+            send_count++;
+            k_sem_give(&threadB_sem);  // Signal threadC
+        }
+        k_sleep(K_SECONDS(10));
+    }
 
-	}
+    printk("Entering low-power mode\n");
+    k_sleep(K_FOREVER);
 }
 
+void threadC(void) {
+    printk("threadC started\n");
+
+    if (!device_is_ready(led_spec.port)) {
+        printk("Error: LED device is not ready\n");
+        return;
+    }
+
+    int ret = gpio_pin_configure_dt(&led_spec, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        printk("Error: Failed to configure LED GPIO pin\n");
+        return;
+    }
+
+    while (1) {
+        k_sem_take(&threadB_sem, K_FOREVER);  // Wait for signal from threadB
+        struct sensor_value last_temp = temp_readings[(reading_index - 1) % 10];
+        if (last_temp.val1 > 30) {  // Temperature is over 30 degrees
+            gpio_pin_set_dt(&led_spec, 1);  // Turn on the LED
+        } else {
+            gpio_pin_set_dt(&led_spec, 0);  // Turn off the LED
+        }
+    }
+}
+
+// Define and initialize threads
 K_THREAD_DEFINE(threadA_id, STACKSIZE, threadA, NULL, NULL, NULL, THREADA_PRIORITY, 0, 0);
 K_THREAD_DEFINE(threadB_id, STACKSIZE, threadB, NULL, NULL, NULL, THREADB_PRIORITY, 0, 0);
 K_THREAD_DEFINE(threadC_id, STACKSIZE, threadC, NULL, NULL, NULL, THREADC_PRIORITY, 0, 0);
 
-
+/* Main function required to enable USB communication */
 int main() {
-	if (usb_enable(NULL)) {
-		return 0;
-	}
 
-	k_busy_wait(5000000);
+    if (usb_enable(NULL)) {
+        printk("Failed to enable USB\n");
+        return 1;
+    }
+
+    int err;
+
+    printk("Starting Bluetooth Peripheral example\n");
+
+    if (IS_ENABLED(CONFIG_BT_LBS_SECURITY_ENABLED)) {
+        err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+        if (err) {
+            printk("Failed to register authorization callbacks.\n");
+            return 1;
+        }
+
+        err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+        if (err) {
+            printk("Failed to register authorization info callbacks.\n");
+            return 1;
+        }
+    }
+
+    err = bt_enable(NULL);
+    if (err) {
+        printk("Bluetooth init failed (err %d)\n", err);
+        return 1;
+    }
+
+    printk("Bluetooth initialized\n");
+
+    if (IS_ENABLED(CONFIG_SETTINGS)) {
+        settings_load();
+    }
+
+    err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (err) {
+        printk("Advertising failed to start (err %d)\n", err);
+        return 1;
+    }
+
+    printk("Advertising successfully started\n");
+
+    /* Busy wait for 5 seconds so you don't miss the initial printk statements to the terminal while it is connecting */
+    k_busy_wait(5000000);
+    return 0;
 }
---
+++ lab3/ex2/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "adafruit_feather_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/dsalvador/Desktop/KU_LEUVEN/Software_for_Real_Time_and_Embedded_Systems/assignment/lab3/ex2/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "app.overlay" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "app.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/4ef6631da0/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
